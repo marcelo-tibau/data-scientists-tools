3fb1d6a01ca3b347e31cf4bfc4350dbd61101a36
Marcelo Tibau <marcelo@tibau.org>
2016-12-21 16:00:04 -0200

    Add files via upload

--- map_assignment.R
+++ map_assignment.R
@@ -2,14 +2,16 @@ library('leaflet')
 
 RJ_Icon <- makeIcon(
   iconUrl = "https://tibaudotorg.files.wordpress.com/2016/12/location_orange_icon.png",
-  iconWidth = 31*215/230, iconHeight = 31,
-  iconAnchorX = 31*215/230/2, iconAnchorY = 16
+  iconWidth = 65*215/230, iconHeight = 65,
+  iconAnchorX = 65*215/230/2, iconAnchorY = 65
 )
 
+#https://tibaudotorg.files.wordpress.com/2016/12/location_orange_icon.png
+# https://tibaudotorg.files.wordpress.com/2016/12/sun-rays-small.png
 # Christ the Redeemer (statue), Sugarloaf Mountain, Copacabana Beach and Maracana Stadium
 RJ_Lat_Long <- data.frame(
   lat = c(-22.9519, -22.9493, -22.9711, -22.9121),
-  lng = C(-43.2105, -43.1546, -43.1825, -43.2302)
+  lng = c(-43.2105, -43.1546, -43.1825, -43.2302)
 )
 
 RJ_Sites <- c(
