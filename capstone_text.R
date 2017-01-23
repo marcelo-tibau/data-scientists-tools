7b87c8ae25b43f886b0a1f7a2c89f06aa2af8c00
Marcelo Tibau <marcelo@tibau.org>
2017-01-23 16:46:44 -0200

    Add files via upload

--- capstone_text.R
+++ capstone_text.R
@@ -3,6 +3,8 @@ library('RWeka')
 library('tm')
 library('SnowballC')
 library('wordcloud')
+library('stringi')
+library('stringr')
 
 News <- readLines("en_US.news.txt", encoding = "UTF-8", skipNul=TRUE)
 Twitter <- readLines("en_US.twitter.txt", encoding = "UTF-8", skipNul = TRUE)
@@ -19,8 +21,28 @@ News.sample <- sample(News, length(News)*0.10, replace = FALSE)
 
 Blogs.sample <- sample(Blogs, length(Blogs)*0.10, replace = FALSE)
 
+# Inspecting the provided Datasets
+
 summary(nchar(News.sample))
 hist(News.sample)
+
+# some info about the data (MB and lines) and summaries 
+format(object.size(News), units = "MB")
+length(News)
+
+format(object.size(Twitter), units = "MB")
+length(Twitter)
+
+format(object.size(Blogs), units = "MB")
+length(Blogs)
+
+summary(Blogs)
+summary(nchar(Blogs))
+summary(Twitter)
+summary(nchar(Twitter))
+summary(News)
+summary(nchar(News))
+
 
 ## Task 1: Getting and cleaning the data
 
@@ -58,6 +80,14 @@ corpus.twitter <- tm_map(corpus.twitter, removePunctuation)
 corpus.twitter <- tm_map(corpus.twitter, tolower)
 corpus.twitter <- tm_map(corpus.twitter, removeWords, stopwords("english"))
 
+# Remove profanity and rude words. From the dataset FrontGateMedia (font: www.FrontGateMedia.com) which presents more than 700 such words.
+# These words will be used as stopwords. 
+
+profanity <- read.csv("Terms-to-Block.csv")
+profanity <- profanity[-c(1:3),]
+profanity <- rep(profanity$Your.Gateway.to.the.Chrisitan.Audience)
+
+corpus <- tm_map(corpus, removeWords, profanity)
 
 
 ## Task 2: Exploratory Data Analysis
@@ -68,8 +98,9 @@ two.gram.toke <- NGramTokenizer(corpus.twitter, Weka_control(min = 2, max = 2))
 two.g <- data.frame(table(two.gram.toke))
 two.g.sort <- two.g[order(two.g$Freq, decreasing = TRUE),]
 
+two.g.sort[1:20,]
+
 
 wordcloud(two.g.sort[,1], freq = two.g.sort[,2], scale = c(5,1), random.order = F, rot.per = 0.5, min.freq = 100, colors = brewer.pal(8, "Dark2"))
-
 
 
