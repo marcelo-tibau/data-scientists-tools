41a659a7c0a6d191bf90510d187197c3e7af170a
Marcelo Tibau <marcelo@tibau.org>
2017-01-30 17:37:35 -0200

    Add files via upload

--- capstone_text_n-gram_models.R
+++ capstone_text_n-gram_models.R
@@ -1,6 +1,62 @@
 library('tm')   
 library('filehash')
 
+# Using virtual corpus to read the three data sets (I created 3 folders and associated one corpus per folder:
+
+Twitter <- VCorpus(DirSource("twitter", encoding = "UTF-8"), readerControl = list(language="en"))
+
+Blogs <- VCorpus(DirSource("blogs", encoding = "UTF-8"), readerControl = list(language="en"))
+
+News <- VCorpus(DirSource("news", encoding = "UTF-8"), readerControl = list(language="en"))
+
+# To split corpora into train, devtest, and test sets I decided to randomize the order of each corpus and 
+# then set the ratio to 60% in order to get the training set and divide the rest into testing and devtest sets.
+
+# Twitter
+set.seed(148)
+perm.twitter <- sample(Twitter[[1]][[1]], length(Twitter[[1]][[1]]))
+TwitR <- round(0.6*length(perm.twitter))
+twitterTrain <- perm.twitter[1:TwitR]
+remain <- perm.twitter[-(1:TwitR)]
+
+DEV <- round(0.5*(length(remain)))
+twitterDevTest <- remain[1:DEV]
+twitterTest <- remain[-(1:DEV)]
+
+write(twitterTrain, "twitterTrain.txt")
+write(twitterDevTest, "twitterDevTest.txt")
+write(twitterTest, "twitterTest.txt")
+
+# Blogs
+set.seed(149)
+perm.blog <- sample(Blogs[[1]][[1]], length(Blogs[[1]][[1]]))
+BlogR <- round(0.6*length(perm.blog))
+blogsTrain <- perm.blog[1:BlogR]
+remain2 <- perm.blog[-(1:BlogR)]
+
+DEV2 <- round(0.5*length(remain2))
+blogDevTest <- remain2[1:DEV2]
+blogTest <- remain[-(1:DEV2)]
+
+write(blogsTrain, "blogsTrain.txt")
+write(blogDevTest, "blogDevTest.txt")
+write(blogTest, "blogTest.txt")
+
+# News
+set.seed(150)
+perm.news <- sample(News[[1]][[1]], length(News[[1]][[1]]))
+NewsR <- round(0.6*length(perm.news))
+newsTrain <- perm.news[1:NewsR]
+remain3 <- perm.news[-(1:NewsR)]
+
+DEV3 <- round(0.5*length(remain3))
+newsDevTest <- remain3[1:DEV3]
+newsTest <- remain3[-(1:DEV3)]
+
+write(newsTrain, "newsTrain.txt")
+write(newsDevTest, "newsDevtest.txt")
+write(newsTest, "newsTest.txt")
+
 #### restart from executive Summary: https://github.com/jgendron/datasciencecoursera/blob/master/NLP-A%20Model%20to%20Predict%20Word%20Sequences.Rmd
 
 profanity <- read.csv("Terms-to-Block.csv")
